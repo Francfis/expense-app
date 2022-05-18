8578398a54d517640e6f1c1ab4de04bf1e1287be
Francfis <fran_p@outlook.com>
2022-05-18 20:04:55 +0200

    cambios para nuestra bbdd

--- config/config.php
+++ config/config.php
@@ -1,11 +1,12 @@
 <?php
 
-define('URL', 'http://localhost:8080/expense-app/');
+// define('URL', 'http://localhost:8080/expense-app/');
+define('URL', 'http://localhost/expenseapp/');
 
 define('HOST', 'localhost');
 define('DB', 'expenseapp');
-define('USER', 'root');
-define('PASSWORD', "root");
+define('USER', 'dwes');
+define('PASSWORD', "abc123.");
 define('CHARSET', 'utf8mb4');
 
 ?>
